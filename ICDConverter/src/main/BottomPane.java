554cb83d068b2c1b861b3eca163a49dceabf213d
senih <senih@10.0.0.135>
2020-10-06 18:14:34 -0400

    top, bottom, center button added

---
+++ ICDConverter/src/main/BottomPane.java
@@ -0,0 +1,19 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class BottomPane extends JPanel{
+
+	BottomPane(int width, int height){
+		
+		//set width same size as MainFrame, set height to 40% of height
+		Dimension d = new Dimension(width, ((int)((double)height*.4)));
+		this.setPreferredSize(d);
+		this.setBackground(Color.BLUE);
+		
+	}
+	
+}
--- ICDConverter/src/main/MainFrame.java
+++ ICDConverter/src/main/MainFrame.java
@@ -1,20 +1,50 @@
 package main;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 
 public class MainFrame extends JFrame{
-
-	public MainFrame() {
+	
+	private BottomPane bottomPane;
+	private TopPane topPane;
+	private CustomButton convertButton;
+	
+	private int width;
+	private int height;
+	
+	MainFrame() {
 		super("ICDConverter");
 		
 		setLayout(new BorderLayout());
 		
+		this.width = 200;
+		this.height = 400;
+		
 		// (x, y)
-		setSize(600, 600);
-		setMinimumSize(new Dimension(1200, 600));
+		Dimension d = new Dimension(width, height);
+		setSize(d);
+		setMinimumSize(d);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//inserting top and bottom panes
+		bottomPane = new BottomPane(width, height);
+		add(bottomPane, BorderLayout.SOUTH);
+		
+		topPane = new TopPane(width, height);
+		add(topPane, BorderLayout.NORTH);
+		
+		//inserting button
+		convertButton = new CustomButton();
+		add(convertButton, BorderLayout.CENTER);
+		
+		convertButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Hello");
+			}
+		});
 		
 		this.pack();
 		this.setLocationRelativeTo(null);
